39d343671cc86c27140f62e4acf26ed98f7270b9
Blueocean <chosukhyun73@gmail.com>
2026-05-27 22:57:10 +0900

    Initial Commit

---
+++ Pharos/PharosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PharosApp.swift
+//  Pharos
+//
+//  Created by Macbook Pro M5 on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct PharosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
